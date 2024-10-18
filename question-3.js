e80585fb88dd4ca4f22089d3201afd0ac3b19321
OLEANG <wannachok.job@gmail.com>
2024-10-18 14:29:17 +0700

    oleang-answer for skillcheck2

--- question-3.js
+++ question-3.js
@@ -2,12 +2,12 @@
 
 const getUsers = () => fetch("https://jsonplaceholder.typicode.com/users");
 
-const onSuccess = (data) => {
-    const name = data.map((user) => user.name)
-    console.log(name);}
+const onSuccess = async(data) => {
+    const response = await data.json();
+    const name = response.map((user) => user.name);
+    console.log(name);
+}
 
-const handleResponse = (Response) => Response.json();
-
-getUsers().then(handleResponse).then(onSuccess)
+getUsers().then(onSuccess)
 
 
